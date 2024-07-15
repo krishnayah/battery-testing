bb645a1f85fc5760ce2bb8b3d4af30dffcc2cb4d
Jordan <139855911+krishnayah@users.noreply.github.com>
2024-07-15 01:33:58 -0400

    Added support for draining using folding@home, created logging system

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import time
 import requests
 import psutil
+import os
 
-LOW = 20
-HIGH = 80
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-on_url = "https://api-v2.voicemonkey.io/trigger?token=GIBBERISH58e70e19&device=turn-on-smart-plug"
-off_url = "https://api-v2.voicemonkey.io/trigger?token=GIBBERISH&device=turn-off-smart-plug"
+driver = webdriver.Chrome()
+driver.get("https://v8-3.foldingathome.org/machines")
+
+LOW = 3
+HIGH = 75
+
+on_url = "https://api-v2.voicemonkey.io/trigger?token=e193146cf5cf43339a19aa5556506bd0_14122fd974d7274e08d46f0258e70e19&device=turn-on-smart-plug"
+off_url = "https://api-v2.voicemonkey.io/trigger?token=e193146cf5cf43339a19aa5556506bd0_14122fd974d7274e08d46f0258e70e19&device=turn-off-smart-plug"
 
 def get_battery_percentage():
     battery = psutil.sensors_battery()
@@ -19,17 +26,35 @@ def power_off():
     requests.get(off_url)
 
 
+def turn_folding_on():
+    driver.get("https://v8-3.foldingathome.org/machines")
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div[2]/a[1]").click()
+
+def turn_folding_off():
+    driver.get("https://v8-3.foldingathome.org/machines")
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div[2]/a[2]").click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/div[3]/a[1]").click()
+
+minutes = 0
+interval = 30
 while True:
+    battery = get_battery_percentage()
+    print(battery)
+    # log battery and time to log.csv
+    with open('log.csv', 'a') as f:
+        f.write(f"{minutes},{battery}\n")
 
-    while True:
-        time.sleep(30)
-        battery = get_battery_percentage()
-        print(battery)
-        if battery >= HIGH:
-
-            power_off()
-
-        elif battery <= LOW:
-            power_on()
+    if battery >= HIGH:
+        power_off()
+        turn_folding_on()
 
 
+    elif battery <= LOW:
+        power_on()
+        turn_folding_off()
+
+    time.sleep(interval)
+    minutes += interval / 60
